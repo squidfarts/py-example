ba0a4792cec73d3745be777263c480f3dba7f559
Micheal Brockus <42789598+squidfarts@users.noreply.github.com>
2019-07-23 18:24:10 -0700

    Add files via upload

---
+++ src/main/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+###################################################################################
+#                                                                                 #
+# NAME: main.py                                                                   #
+#                                                                                 #
+# AUTHOR: Michael Brockus.                                                        #
+#                                                                                 #
+# CONTACT: <mailto:michael@squidfarts.com>                                        #
+#                                                                                 #
+# NOTICES:                                                                        #
+#                                                                                 #
+# License: MIT                                                                    #
+#                                                                                 #
+###################################################################################
+
+
+
+def main():
+    print("Hello, Python3.")
+
+# end of function main
+
+if __name__ == "__main__":
+    main()
